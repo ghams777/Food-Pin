eb9fee4de31ac0ad685129fa1ebfb8efe64e86be
Managam Silalahi <managam.silalahi@gmail.com>
2016-04-06 11:41:34 +0700

    Mengoptimasi Performa Aplikasi dengan Activity Indicator dan Lazy Loading.

--- FoodPin/FeedTableViewController.swift
+++ FoodPin/FeedTableViewController.swift
@@ -95,7 +95,7 @@ class FeedTableViewController: UITableViewController {
         
         // Create the query operation with the query
         let queryOperation = CKQueryOperation(query: query)
-        queryOperation.desiredKeys = ["name", "image"]
+        queryOperation.desiredKeys = ["name"]
         queryOperation.queuePriority = .VeryHigh
         queryOperation.resultsLimit = 50
         
